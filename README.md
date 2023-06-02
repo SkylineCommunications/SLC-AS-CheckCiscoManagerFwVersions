12f849e56e26c256d56969e3a2041ed57a228699
João Silva <joao.silva@skyline.be>
2023-06-02 10:39:10 +0100

    Initial Solution creation.

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # SLC-AS-CheckCiscoManagerFwVersions
-Used to get DataMiner version and Cisco Manager Software Description of every element in DMS
---
+++ SLC-AS-CheckCiscoManagerFwVersions_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SLC-AS-CheckCiscoManagerFwVersions_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("SLC-AS-CheckCiscoManagerFwVersions_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("0057D8D2-06E0-48A0-BA79-254C8977BBE7")]
+[assembly: AssemblyVersion("1.0.0.0")]
